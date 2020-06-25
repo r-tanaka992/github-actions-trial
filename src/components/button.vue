821a553def8534b6efab8a55c28502ca25247a24
r-tanaka992 <tanaka.r@hc-tech.jp>
2020-06-25 17:48:54 +0900

    :sparkles: ボタンコンポーネントの作成

---
+++ src/components/button.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="button">
+    <button>{{ label }}</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Button',
+  props: {
+    label: String
+  }
+}
+</script>
